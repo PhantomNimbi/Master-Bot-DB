41bc899f93a6e13055315501f489c4a4940ae579
PhantomNimbi <Darkwater409@gmail.com>
2026-08-18 09:40:30 -0700

    Update init-multiple-databases.sh

--- init-multiple-databases.sh
+++ init-multiple-databases.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Default to master_bot if the DB_NAME variable is empty
+MAIN_DB="${DB_NAME:-master_bot}"
+SHADOW_DB="${MAIN_DB}_shadow"
+
+echo "Creating shadow database: ${SHADOW_DB}"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE master_bot_shadow;
+    CREATE DATABASE ${SHADOW_DB};
 EOSQL
